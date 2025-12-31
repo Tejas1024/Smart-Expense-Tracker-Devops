dcd1742f49c10c7387b3569a1b58b606503908dd
Tejas1024 <tejaspavithra2002@gmail.com>
2025-12-31 16:44:42 +0530

    Add GitHub Actions CI workflows

--- backend/authentication/apps.py
+++ backend/authentication/apps.py
@@ -20,4 +20,4 @@ from django.apps import AppConfig
 class AiInsightsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ai_insights'
-    label = 'ai_insights'
+    label = 'ai_insights' 
